83cd92a0b74dbc57040822d95adf38cc134ab2f9
Recai Oktaş <roktas@gmail.com>
2020-01-05 13:42:42 +0300

    .

--- _.sh
+++ _.sh
@@ -1,7 +1,10 @@
 #=github.com/omu/home/src/sh/!.sh
+#=github.com/omu/home/src/sh/callback.sh
+#=github.com/omu/home/src/sh/defer.sh
+#=github.com/omu/home/src/sh/temp.sh
 
 main() {
-	echo "$@"
+	echo "$@" # hmm
 }
 
 main "$@"
